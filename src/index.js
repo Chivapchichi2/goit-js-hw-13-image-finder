54323dcbab008c6a56bcda7217234587b0c8a241
Pavel Ivchenko <71591602+Chivapchichi2@users.noreply.github.com>
2021-03-24 00:11:40 +0200

    basic logick api request

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
+import './js/apiService';
 import './styles.css';
+
+// https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=cat&page=1&per_page=12&key=20826556-19d7dce6dc96816ed1b7dccf7
